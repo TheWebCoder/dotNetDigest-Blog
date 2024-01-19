3dfb4678e9675fe7ee9f3eaaf0b482453516b4dc
Bryan <127161500+TheWebCoder@users.noreply.github.com>
2024-01-18 16:10:25 -0800

    Added logo and navigation links for tags

---
+++ dotNetDigest.Web/ViewComponents/Tag/TagViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using dotNetDigest.Web.Models.Domain; // Assuming Tag is here
+using dotNetDigest.Web.Repositories;
+
+public class TagViewComponent : ViewComponent
+{
+    private readonly ITagRepository _tagRepository;
+
+    public TagViewComponent(ITagRepository tagRepository)
+    {
+        _tagRepository = tagRepository;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var tags = await _tagRepository.GetAllAsync();
+        return View(tags); // Make sure 'Tag' is recognized as a type here
+    }
+}
