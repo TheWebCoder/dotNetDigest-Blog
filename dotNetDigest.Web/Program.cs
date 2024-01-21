6899ce0c484e2c5a74369ee94ca85290d1b2ea0c
Bryan <127161500+TheWebCoder@users.noreply.github.com>
2024-01-20 20:30:21 -0800

    Removed froala and implemented TinyMCE.

--- dotNetDigest.Web/Program.cs
+++ dotNetDigest.Web/Program.cs
@@ -54,13 +54,15 @@ namespace dotNetDigest.Web
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            //if (!app.Environment.IsDevelopment())
+            //{
+            //    app.UseExceptionHandler("/Error");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
 
+
+            app.UseDeveloperExceptionPage();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
